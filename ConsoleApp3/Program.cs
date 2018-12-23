4d4ee95453f907dba9dd646806161d7e9823990e
Нурбек Джабашев <invokedex@gmail.com>
2018-12-23 21:21:39 +0600

    First project

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -4,12 +4,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp3
+namespace ConsoleApp1
 {
     class Program
     {
         static void Main(string[] args)
         {
+            string name = "Джабашев Нурбек";
+            Console.WriteLine(name);
+            Console.WriteLine("--------------------------------------------------");
+            const int number = 3;
+            int[] arr = new int[number];
+            Random rand = new Random();
+            int i = 0, Sum = 0;
+            for (; i < number; i++)
+            {
+                arr[i] = rand.Next(0, 10);
+                Console.WriteLine(arr[i]);
+                Sum += arr[i];
+            }
+            Console.WriteLine("Сумма равна = " + Sum);
+            Console.ReadLine();
         }
     }
 }
